359657c9692200d500dd84cd30a86c0d02e65816
Aso200 <asmir_ramiz@hotmail.com>
2023-01-16 13:25:01 +0100

    klar

--- app.js
+++ app.js
@@ -1,2 +1,4 @@
-var djur = ["apa", "orangutang", "gorilla", "get", "giraff"];
-console.log(djur);
+//let djur : string[] = ["apa", "orangutang", "gorilla", "get", "giraff" ];
+//console.log(djur);
+var value = ["aso", 2, true];
+console.log(value);
--- app.ts
+++ app.ts
@@ -1,3 +1,9 @@
-let djur : string[] = ["apa", "orangutang", "gorilla", "get", "giraff" ];
+//let djur : string[] = ["apa", "orangutang", "gorilla", "get", "giraff" ];
 
-console.log(djur);
+//console.log(djur);
+
+
+let value: (string | number | boolean) [] = ["aso", 2, true ];
+
+
+console.log(value)
