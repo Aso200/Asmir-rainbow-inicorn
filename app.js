06cd151d33600e30271124fadd28c70966ffa175
Aso200 <asmir_ramiz@hotmail.com>
2023-01-17 12:09:17 +0100

    klar

--- app.js
+++ app.js
@@ -1,4 +1,12 @@
-//let djur : string[] = ["apa", "orangutang", "gorilla", "get", "giraff" ];
-//console.log(djur);
+var djur = ["apa", "orangutang", "gorilla", "get", "giraff"];
+console.log(djur);
 var value = ["aso", 2, true];
 console.log(value);
+function greet(x) {
+    return "hej ".concat(x);
+}
+console.log(greet("aso"));
+for (var i in djur) {
+    console.log(greet(djur[i]));
+}
+;
--- app.ts
+++ app.ts
@@ -1,9 +1,23 @@
-//let djur : string[] = ["apa", "orangutang", "gorilla", "get", "giraff" ];
+let djur : string[] = ["apa", "orangutang", "gorilla", "get", "giraff" ];
 
-//console.log(djur);
-
+console.log(djur);
 
 let value: (string | number | boolean) [] = ["aso", 2, true ];
 
+console.log(value);
 
-console.log(value)
+
+function greet (x:string): string{
+    return `hej ${x}`
+}
+
+console.log(greet("aso"));
+
+for (let i in djur) {
+    console.log(greet(djur[i])
+    
+)};
+
+
+
+
